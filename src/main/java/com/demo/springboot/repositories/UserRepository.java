e04761fda41ad2ab6113b77902a3b8b5926bd243
Saurabh Landge <slsaurabh25@gmail.com>
2017-08-19 02:03:21 +0530

    Initial commit

---
+++ src/main/java/com/demo/springboot/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.demo.springboot.repositories;
+
+import com.demo.springboot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByName(String name);
+
+}
